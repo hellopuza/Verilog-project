a0c9f3e012501b924705f45ed99f2e54d4dad062
Ger0r0r <fedor.bombax@gmial.com>
2023-05-04 07:20:04 +0300

    rename module rand

---
+++ get_random.v
@@ -0,0 +1,25 @@
+module get_random
+#(
+    parameter   MODULUS = 10,
+    parameter   NBITS   = $clog2(MODULUS)
+)
+(
+    input               clk,
+    input               rst,
+    output  [NBITS-1:0] number
+);
+
+counter
+#(
+    .MODULUS    (MODULUS)
+) counter_counter
+(
+    .clk        (clk),
+    .rst        (rst),
+    .incr       (1'd1),
+    .number     (number),
+    .set_data   (),
+    .data       ()
+);
+
+endmodule
