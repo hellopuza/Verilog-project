4494974fdbdb2eed30e9e92ad4b72b8f8ad48996
Ger0r0r <fedor.bombax@gmial.com>
2023-05-07 22:08:15 +0300

    rework apple gen

--- possible_apple.v
+++ possible_apple.v
@@ -9,15 +9,15 @@ module possible_apple
     input	wire    [SBITS-1:0] 		seed,
     input	wire 	[FIELD_SIZE-1:0]    field,
 
-	output	wire	[(SBITS+1)*SIZE_X*SIZE_Y-1:0]	sets_seed
+	output	wire	[SBITS*SIZE_X*SIZE_Y-1:0]	sets_seed
 );
 
-assign sets_seed[(SBITS+1)*SIZE_X*SIZE_Y-1:SBITS*SIZE_X*SIZE_Y] = SIZE_X*SIZE_Y-1;
+assign sets_seed[SBITS*SIZE_X*SIZE_Y-1:(SBITS-1)*SIZE_X*SIZE_Y] = SIZE_X*SIZE_Y-1;
 
 genvar Gi;
-generate for (Gi = 0; Gi < SIZE_X*SIZE_Y; Gi = Gi + 1)
+generate for (Gi = 1; Gi < SIZE_X*SIZE_Y; Gi = Gi + 1)
 	begin: loop1
-		assign sets_seed[(Gi+1)*SBITS-1:Gi*SBITS] = (field[(Gi+1)*3-1:Gi*3] == 3'd0) ? Gi : sets_seed[(Gi+2)*SBITS-1:(Gi+1)*SBITS];
+		assign sets_seed[(Gi+1)*SBITS-1:Gi*SBITS] = (field[(Gi+1)*3-1:Gi*3] == 3'd0) ? Gi : sets_seed[(Gi)*SBITS-1:(Gi-1)*SBITS];
 	end
 endgenerate
 
